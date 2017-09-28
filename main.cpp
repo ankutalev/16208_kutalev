f518dc863c3080ae6d5211c310212c82673b2ae5
ankutalev <31923880+ankutalev@users.noreply.github.com>
2017-09-28 22:52:00 +0700

    main.cpp
    Hashtable

--- main.cpp
+++ main.cpp
@@ -1,117 +1,398 @@
 #include <iostream>
-#include <stdlib.h>
-#include<stdio.h>
-#include<assert.h>
+#include <string>
+#include <assert.h>
+
 using namespace std;
-class Array {
-    private:
-        int* data;
-        int size;
-        int capacity;
-       const static int default_capacity = 10;
-    public:
-        Array(int); // constructor
-        ~Array();                    // destructor
-        Array(const Array&);  // copy constructor;
-        Array(); // default constructor
-        Array& operator = (const Array&); //copy  assignment
-        void AddElement(int);
-       // void ShowArray();
-        friend ostream& operator<<(ostream& os, const Array& t); // cout << Array
-        Array operator +(const Array&); // array z = array x [A1,A2.....Asize] + array y [B1...Bsize] = [A1...Asize,B1.. Bsize]
-        int &operator [](int i);
+typedef string Key;
+
+const int HashConst = 31;
+
+const int HighLoadFactor = 2;
+
+
+size_t HashCalculation (const Key& in) {
+    size_t length = in.length();
+    size_t HashValue = in[length-1] % HashConst;
+    for(int i=length-2;i>=0;i--)
+        HashValue= HashValue*HashConst+in[i]%HashConst;
+    return HashValue;
+}
+struct Value {
+    unsigned age;
+    unsigned weight;
+    Value(int,int);
+    Value();
 };
-Array::Array(){
-    data=(int*)calloc(default_capacity,sizeof(int));
-    capacity=default_capacity;
-    size=0;
+Value::Value () {
+    age = 20;
+    weight = 75;
 }
-Array::Array (int initial_capacity) {
-    assert(initial_capacity > 0);
-    data = (int*)calloc(initial_capacity,sizeof(int));
-    assert(data);
-    size=0;
-    capacity = initial_capacity;
+Value::Value (int a, int w):age(a),weight(w) {
 }
-Array::Array(const Array& t) {
-    data = (int*)calloc(t.capacity,sizeof(int));
-    assert(data);
-    size = t.size;
-    capacity = t.capacity;
-    for (int i = 0; i < size;i++)
-        data[i]=t.data[i];
+struct Node {
+        Value Current;
+        Key CurrentKey = "Default Anonymous";
+        Node* Next = nullptr;
+        Node(const Key&,const Value&);
+};
+Node::Node(const Key& k ,const Value& t) {
+    CurrentKey = k;
+    Current.age = t.age;
+    Current.weight = t.weight;
+    Next = nullptr;
 }
-Array& Array:: operator =(const Array& t) {
-    if (this!= &t) {
-        if (data)
-            delete data;
-        data = (int*)calloc(t.capacity,sizeof(int));
-        assert(data);
-        capacity = t.capacity;
-        size = t.size;
-        for (int i =0;i<t.size;i++)
-            data[i]=t.data[i];
+class EntriesList {
+    public:
+        EntriesList();
+       ~EntriesList(); // destructor
+        EntriesList& operator=(const EntriesList&);// new assignment
+        EntriesList(const EntriesList&); // copy constructor;
+        bool addElement(const Key&,const Value&); // add element to the end of list
+        bool Erase(const Key&); // remove element from list
+        void showList();
+        bool Contains(const Key&) const; // checking value's availability with key
+        bool IsEmpty () const;
+        Node& GetFirst();
+        Value& At (const Key&) const;
+        bool Implace (const Key& k , const Value& v);
+        friend bool operator == (const EntriesList&,const EntriesList&);
+    private:
+        Node* head;
+};
+bool operator == (const EntriesList& t1, const EntriesList& t2) {
+    Node* tmp = t1.head;
+    while (tmp) {
+        if(!t2.Contains(tmp->CurrentKey))
+            return false;
+            tmp=tmp->Next;
+    }
+    tmp = t2.head;
+    while (tmp) {
+        if(!t1.Contains(tmp->CurrentKey))
+            return false;
+            tmp=tmp->Next;
+    }
+    return true;
+}
+bool operator != (const EntriesList& t1, const EntriesList& t2) {
+    return !(t1==t2);
+}
+Value& EntriesList::At(const Key& k) const {
+    Node* tmp = head;
+    while(tmp) {
+        if(tmp->CurrentKey==k)
+            return tmp->Current;
+        tmp=tmp->Next;
+    }
+    if (!tmp)
+        throw new exception;
+}
+
+bool EntriesList::IsEmpty() const {
+    return (head) ? false : true;
+}
+Node& EntriesList :: GetFirst() {
+    Node* tmp = head;
+    head=head->Next;
+    return *tmp;
+}
+bool EntriesList::Contains (const Key& k) const  {
+    Node* tmp = head;
+    while(tmp) {
+        if (tmp->CurrentKey==k)
+            return true;
+        tmp=tmp->Next;
+    }
+    return false;
+}
+EntriesList:: EntriesList() {
+    head = nullptr;
+}
+EntriesList:: EntriesList(const EntriesList&t) {
+    head = nullptr;
+    Node* tmp = t.head;
+    while (tmp){
+        this->addElement(tmp->CurrentKey,tmp->Current);
+        tmp=tmp->Next;
     }
 }
-ostream& operator<<(ostream& os, const Array& t) {
-    for (int i = 0; i<t.size; i++)
-        os<<t.data[i]<<" ";
-    return os;
+EntriesList& EntriesList:: operator = (const EntriesList& t) {
+    if (this!= &t) {
+        Node* tmp = t.head;
+        delete this; //???????????????????????????? нужно ли разделять деструктор и освобождение ресурсов если по сути для освобождения старых ресурсов нужно удалить старый объект
+        while (tmp) {
+            this->addElement(tmp->CurrentKey,tmp->Current);
+            tmp=tmp->Next;
+        }
+    }
+    return *this;
 }
-Array::~Array() {
-    if(data)
-        delete data;
+EntriesList:: ~EntriesList() {
+        while(head) {
+            Node* tmp= head;
+            head=head->Next;
+            delete tmp;
+        }
 }
-void Array:: AddElement(int elem) {
-    assert(data);
-    if (size < capacity) {
-        data[size] = elem;
-        size++;
+bool EntriesList :: addElement(const Key& k, const Value& info) {
+    if (!head) {
+        head = new Node(k,info);
+        if (!head)
+            return false;
     }
     else {
-        capacity=2*capacity;
-        data = (int*)realloc(data,sizeof(int)*capacity);
-        assert(data);
-        data[size]=elem;
-        size++;
+        Node* tmp = head;
+        while(tmp->Next)
+            tmp=tmp->Next;
+        tmp->Next = new Node(k,info);
+        if (!tmp->Next)
+            return false;
+    }
+     return true;
+}
+bool EntriesList::Implace(const key& k , const value& v) {
+
+}
+void EntriesList :: showList() {
+    if(!head)
+        cout<<"Empty list"<<endl;
+    Node* tmp = head;
+    while (tmp) {
+        cout<<"Name:"<< tmp->CurrentKey << endl << "Age:" << tmp->Current.age << endl << "Weight:" << tmp->Current.weight << endl;
+        tmp=tmp->Next;
     }
 }
-/*void Array:: ShowArray() {
-    for(int i =0;i< size;i++)
-        cout<<data[i]<<" ";
-} */
-int& Array::operator[](int i) {
-    assert(i<size);
-    return data[i];
+
+bool EntriesList :: Erase (const Key& k ) {
+    if (!head)
+        return false;
+    Node* tmp = head;
+    if (tmp->CurrentKey==k) {
+        head=head->Next;
+        delete tmp;
+        return true;
+    }
+    while (tmp->Next) {
+        if (tmp->Next->CurrentKey==k) {
+            Node* tmp1= tmp->Next;
+            tmp->Next=tmp1->Next;
+            delete tmp1;
+            return true;
+        }
+        tmp=tmp->Next;
+    }
+    return false;
 }
-Array Array:: operator + (const Array& t1) {
-    Array Z;
-    Z.capacity=capacity+t1.capacity;
-    Z.size = t1.size + size;
-    Z.data = (int*)calloc(Z.capacity,sizeof(int));
-    assert(Z.data);
-    int i,j;
-    for(i =0;i<size;i++)
-        Z.data[i] = data[i];
-    for(j=0;j < t1.size;j++)
-        Z.data[i+j] = t1.data[j];
-    return Z;
+
+class HashTable {
+    public:
+        HashTable(); // default constructor
+        ~HashTable();// destructor
+        HashTable(size_t); // constuctor with any capacity
+        HashTable(const HashTable&); // assignment constructor
+        HashTable(HashTable&&); // move constructor;
+        void Swap(HashTable&);// swapp values
+        HashTable& operator =(const HashTable&); // new assignment
+        HashTable& operator = (HashTable&&); // move operator;
+        void Clear(); // clear fields
+        bool Erase(const Key&); //  erase field with key ; true if success
+        bool Insert(const Key& , const Value&); // add field ; true if success
+        bool Contains(const Key&) const; // checking value's availability with key
+        Value& operator[](const Key&); //return key's  value; UNSAFE method;
+       //  if there is no value for this key, method will add default value and return link to it
+        Value& At(const Key&);// return key's value; if there is no value - throw exception
+       const Value& At(const Key&) const; // return key's value; if there is no value - throw exception
+       size_t Size() const; // return current hashtable's size
+       bool Empty() const; // true if empty
+       void Show();
+       bool Implace (const Key& k , const Value& v);
+       friend bool operator == (const HashTable&,const HashTable&);
+       friend bool operator != (const HashTable&,const HashTable&);
+    private:
+        size_t size = 0;
+        size_t capacity = 2;
+        EntriesList* data = nullptr;
+        int loadFactor = 0;
+        bool ReHashing();
+
+};
+HashTable::HashTable(HashTable&& t) {
+    cout<< "move constructor";
+    data = t.data;
+    size = t.size;
+    loadFactor = t.loadFactor;
+
+
+    t.data = nullptr;
+    t.size=0;
+    t.capacity = 2;
+    t.loadFactor = 0;
+
+}
+HashTable& HashTable:: operator = (HashTable&& t) {
+    cout<< "move operator";
+    if (this!= &t) {
+        delete [] data;
+        data = t.data;
+
+        size = t.size;
+        capacity = t.capacity;
+        loadFactor=t.loadFactor;
+
+        t.data = nullptr;
+    }
+    return *this;
+}
+void HashTable::Clear() {
+    delete [] data;
+    size = 0;
+    capacity = 2;
+    loadFactor = 0;
+    data = new EntriesList[capacity];
+}
+Value& HashTable::operator[](const Key& k) {
+        if(this->Contains(k))
+            return data[HashCalculation(k) % capacity].At(k);
+        else {
+            Value Default;
+            this->Insert(k,Default);
+            return data[HashCalculation(k) % capacity].At(k);
+        }
+}
+bool operator == (const HashTable& t1, const HashTable& t2) {
+    if (t1.size!=t2.size)
+        return false;
+    for (size_t i = 0 ; i< t1.capacity; i++)
+        if(t1.data[i]!=t2.data[i])
+            return false;
+    return true;
+}
+bool operator != (const HashTable& t1, const HashTable& t2) {
+    return !(t1==t2);
+}
+Value& HashTable::At(const Key& k) {
+    if (this->Contains(k))
+        return this->data[HashCalculation(k)%capacity].At(k);
+    else throw new exception;
+}
+HashTable:: HashTable() {
+    size = 0;
+    data = new EntriesList[capacity];
+}
+
+HashTable:: HashTable(size_t c): capacity(c) {
+    data = new EntriesList[capacity];
+}
+
+HashTable&  HashTable :: operator = (const HashTable& t) {
+    if (this!= &t) {
+        delete[] data;
+        size = t.size;
+        capacity = t.capacity;
+        loadFactor = t.loadFactor;
+        data = new EntriesList[capacity];
+        assert(data);
+
+        // std::copy
+        copy(t.data,t.data+t.capacity,data + capacity);
+     /*   for(size_t i = 0; i < capacity ; i++) {
+            data[i] = t.data[i];
+        } */
+    }
+    return *this;
+}
+HashTable::HashTable(const HashTable& t) {
+    size = t.size;
+    capacity = t.capacity;
+    loadFactor = t.loadFactor;
+    data = new EntriesList[capacity];
+    for(size_t i = 0; i < capacity ; i++) {
+        data[i] = t.data[i];
+    }
+}
+
+HashTable:: ~HashTable() {
+    delete []data;
+}
+void HashTable:: Swap(HashTable& t ) {
+
+   size_t tmp = size;
+   size = t.size;
+   t.size = tmp;
+
+   tmp= capacity;
+   capacity=t.capacity;
+   t.capacity=tmp;
+
+   tmp = loadFactor;
+   loadFactor=t.loadFactor;
+   t.loadFactor=tmp;
+
+
+   EntriesList* tmp1 = data;
+   data=t.data;
+   t.data=tmp1;
+
+}
+
+bool HashTable:: Insert(const Key& k, const Value& t) {
+        if (Contains(k))
+            return false;
+    size_t index = HashCalculation(k) % capacity;
+    if(!data[index].addElement(k,t))
+        return false;
+    size++;
+    loadFactor=size/capacity;
+    if (loadFactor >= HighLoadFactor)
+       if (!ReHashing())
+        return false;
+    return true;
+}
+
+bool HashTable:: ReHashing() {
+    HashTable tmp(capacity * HighLoadFactor); // tmp hashtable with more capacity
+    for (size_t i = 0 ;i <capacity; i++) { // clear all lists in old and adding values tmp table
+            while(!data[i].IsEmpty()) {
+                Node ntmp = data[i].GetFirst();
+               if (!tmp.Insert(ntmp.CurrentKey,ntmp.Current))
+                    return false;
+            }
+    }
+    Swap(tmp);
+    return true;
+}
+size_t HashTable::Size() const {
+    return size;
+}
+bool HashTable ::Empty() const {
+   return (size) ? true : false;
+}
+bool HashTable::Contains(const Key& k) const  {
+    size_t index = HashCalculation(k) % capacity;
+    return data[index].Contains(k);
+}
+bool HashTable::Erase(const Key& k) {
+    size_t index = HashCalculation(k) % capacity;
+    return data[index].Erase(k);
+}
+void HashTable::Show () {
+    for (size_t i = 0; i<capacity;i++) {
+            data[i].showList();
+            cout<<endl;
+    }
+}
+bool HashTable:: Implace (const Key& k , const Value& v) {
+    size_t index = HashCalculation(k)% capacity;
+    if (data[index].Implace(k,v))
+        return true;
+    return false;
 }
 int main()
 {
-    Array X;
-    X.AddElement(5);
-    X.AddElement(10500);
-    X.AddElement(3);
-    X.AddElement(3123);
-//    X.ShowArray();
-    Array Y;
-    for(int i =0; i<5;i++)
-        Y.AddElement(i);
-    cout << X << endl << Y << endl;
-    Array Z = X + Y;
-    cout<< Z;
-    Z[0]=322;
-    cout<<endl<<Z;
+    Value a,b,c,d,g;
+ //   HashTable Z = mem();
+
+    //Z = mem();
+   // Z.Show();
     return 0;
 }
