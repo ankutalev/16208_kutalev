98378c0678a5aa9e8056e73c9d576627cf2afa92
ankutalev <31923880+ankutalev@users.noreply.github.com>
2018-06-18 07:49:30 +0300

    уже получше

--- torrent/src/IncomingListener.java
+++ torrent/src/IncomingListener.java
@@ -5,10 +5,7 @@ import java.net.Socket;
 import java.net.SocketTimeoutException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
-import java.nio.channels.SelectionKey;
-import java.nio.channels.Selector;
-import java.nio.channels.ServerSocketChannel;
-import java.nio.channels.SocketChannel;
+import java.nio.channels.*;
 import java.util.*;
 
 public class IncomingListener implements Runnable {
@@ -19,24 +16,20 @@ public class IncomingListener implements Runnable {
             pathesToFiles.load(loadfile);
             Selector selector = Selector.open();
             ServerSocketChannel ssc = ServerSocketChannel.open();
-            ssc.bind(new InetSocketAddress("192.168.0.198",PORT));
+            ssc.bind(new InetSocketAddress("192.168.0.198", PORT));
             ssc.configureBlocking(false);
             int ops = ssc.validOps();
-            ssc.register(selector, ops,null);
+            ssc.register(selector, ops, null);
             int connections;
             ByteBuffer readBuffer = ByteBuffer.allocate(this.ASYNC_BUFF_LENGTH);
             SocketChannel is = null;
             String keyIp = " null";
             while (true) {
                 connections = selector.select(10000);
-                System.out.println("zdras'te");
+//                System.out.println("zdras'te");
+
+//                System.out.println(heh.position());
                 if (connections == 0) {
-                    if (heh.position()==messageLen) {
-                        System.out.println(new String(heh.array()));
-                        System.out.println("krasivo");
-                        System.exit(0);
-                    }
-                    System.out.println(heh.position());
                     System.out.println("waiting for peer");
                     continue;
                 }
@@ -44,13 +37,14 @@ public class IncomingListener implements Runnable {
                 Iterator<SelectionKey> it = keys.iterator();
 
                 while (it.hasNext()) {
+//                    System.out.println("y menya est' next!");
                     SelectionKey key = it.next();
                     if (key.isAcceptable()) {
                         SocketChannel socket = ssc.accept();
                         keyIp = socket.getRemoteAddress().toString();
-                        peersStatus.put(keyIp,true);
+                        peersStatus.put(keyIp, true);
                         socket.configureBlocking(false);
-                        socket.register(selector, SelectionKey.OP_READ );
+                        socket.register(selector, SelectionKey.OP_READ  | SelectionKey.OP_WRITE);
                         System.out.println("got one!");
                     }
 //                    if (key.) {
@@ -58,51 +52,30 @@ public class IncomingListener implements Runnable {
 //                        keys.remove(key);
 //                    }
                     if (key.isReadable()) {
-                            if (peersStatus.get(keyIp)) {
-                                is = (SocketChannel) key.channel();
-                                System.out.println(is);
-                                int len = is.read(readBuffer);
-                                System.out.println(len);
-                                readBuffer.rewind();
-                                messageLen = readBuffer.getInt();
-                                System.out.println(messageLen);
-                                peersStatus.replace(keyIp, false);
+                        System.out.println("YA READEBLE!");
+                        if (peersStatus.get(keyIp)) {
+                            is = (SocketChannel) key.channel();
+                            System.out.println(is);
+                            int len = is.read(readBuffer);
+                            System.out.println(len);
+                            readBuffer.rewind();
+                            messageLen = readBuffer.getInt();
+                            heh = ByteBuffer.allocate(messageLen);
+                            System.out.println(messageLen);
+                            peersStatus.replace(keyIp, false);
+                        } else {
+                            if (heh.hasRemaining()) {
+                                System.out.println("vse ewe kachau!");
+                                is.read(heh);
+                                System.out.println(heh);
+                            } else if (heh.position() == messageLen) {
+                                System.out.println(heh);
+                                heh.clear();
+                                System.out.println("YA SKACHAL!");
+                                is.register(selector, SelectionKey.OP_WRITE);
+                                peersStatus.replace(keyIp, true);
                             }
-                            else {
-                                if (heh.position()==0) {
-                                    System.out.println("KACJAU!");
-                                    System.out.println(heh);
-                                    heh.flip();
-                                    heh.compact();
-
-
-                                    is.read(heh);
-
-                                    System.out.println(heh.position());
-                                    for (int i = 0; i < 20000; i++) {
-                                        System.out.println("kachau");
-                                    }
-                                    System.out.println(heh.position());
-
-
-                                    System.out.println(heh);
-                                    try {
-                                        Thread.sleep(20000);
-                                    } catch (InterruptedException e) {
-                                        e.printStackTrace();
-                                    }
-                                }
-                               else if (heh.position()==messageLen) {
-                                    System.out.println(heh);
-                                    heh.clear();
-                                    System.out.println("YA SKACHAL!");
-                                    peersStatus.replace(keyIp,true);
-                                }
-                                else {
-                                    System.out.println("YA EWE NE SKACHAL!");
-                                    System.out.println(heh.position());
-                                }
-                            }
+                        }
 //                        System.out.println(len);
 //                        switch (len) {
 //                            case 0:
@@ -133,13 +106,37 @@ public class IncomingListener implements Runnable {
 //                        System.out.println(new String(data));
 //                        System.out.println("heh")
                     }
-//                    if (key.isWritable()) {
-//                        SocketChannel is = (SocketChannel) key.channel();
-//                        is.write(ByteBuffer.wrap("sosi huy! like esli bi(d)lo".getBytes()));
-//
-//                    }
-                    it.remove();
+                   else if (key.isWritable()) {
+                        if (heh.position()==messageLen) {
+                            System.out.println("piwu!");
+                            String msg = new String(heh.array());
+                            String[] msgData = msg.split(" ");
+                            heh.clear();
+                            if (msgData.length==1) {
+                                if (pathesToFiles.containsKey(msg)) {
+                                    is.write(ByteBuffer.allocate(4).putInt("ALL".length()));
+                                    is.write(ByteBuffer.wrap("ALL".getBytes()));
+                                }
+                                else {
+                                    is.write(ByteBuffer.allocate(4).putInt("NONE".length()));
+                                    is.write(ByteBuffer.wrap("NONE".getBytes()));
+                                }
+                            }
+                        }
+//                        is = (SocketChannel)key.channel();
+//                       if (is.write(ByteBuffer.wrap("sosi huy! like esli bi(d)lo".getBytes()))==0) {
+//                           System.out.println("can't write!");
+//                           key.cancel();
+//                       }
+//                        try {
+//                            Thread.sleep(5000);
+//                        } catch (InterruptedException e) {
+//                            e.printStackTrace();
+                        }
+////
                 }
+                it.remove();
+            }
 //               try (var is = new BufferedReader(new InputStreamReader(client.getInputStream()));
 //                    var os = new PrintWriter(new OutputStreamWriter( client.getOutputStream()))) {
 //                  var request = is.readLine();
@@ -179,7 +176,6 @@ public class IncomingListener implements Runnable {
 //            exc.printStackTrace();
 //            System.out.println("y tebya chot s sdiom daunom");
 //            System.exit(0);
-            }
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -195,7 +191,7 @@ public class IncomingListener implements Runnable {
     private Map<String, ArrayList<Integer>> tableOfAvailability = new HashMap<>();
     private Map <String,Boolean> peersStatus = new HashMap<>();
     private List<Integer> myPieces = new ArrayList<>();
-    private int messageLen;
+    private int messageLen =-1;
     private final  int PORT = 9875;
     private Properties pathesToFiles = new Properties();
     private final String propertiesFileName = "files.properties";
