2db4cc2f66e38e7aafa4034720a118042a6a3c7f
ankutalev <31923880+ankutalev@users.noreply.github.com>
2018-06-18 13:40:50 +0300

    А там отображается где ты это??

---
+++ Downloader.java
@@ -0,0 +1,173 @@
+import java.io.*;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Downloader {
+    Downloader (String fileName,List<String> ipAdresses, List <FilePiece> filePieces) {
+        try {
+            this.fileName = fileName;
+            this.pieceNumber= filePieces.size();
+            SocketChannel sc;
+            String fullIp;
+            for (String ip: ipAdresses) {
+                sc = SocketChannel.open(new InetSocketAddress(ip,9875));
+                fullIp = sc.getRemoteAddress().toString();
+                sendInfoMessage(sc);
+                receiveMessage(sc);
+                int availablePieces = tableOfAvailablePieces.get(fullIp).size();
+                if(availablePieces>0) {
+                    int nextPieceNumber = getNumberOfNextRecordedPiece(fullIp);
+                    System.out.println(nextPieceNumber);
+                    downloadPiece(sc,filePieces.get(nextPieceNumber));
+                }
+
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("error");
+            System.exit(0);
+        }
+    }
+
+    private void downloadPiece(SocketChannel sc, FilePiece piece)  {
+//       try (FileOutputStream fileWriter = new FileOutputStream("/home/ulyssess/tmp/piece"+piece.getId());
+//            DataInputStream is = new DataInputStream(firstTestSocket.getInputStream());
+//            PrintWriter os = new PrintWriter(new OutputStreamWriter(firstTestSocket.getOutputStream())) ) {
+
+//           os.println(fileName+" "+piece.getId()*pieceLength+" "+piece.getLength());
+//           os.flush();
+           byte data[] = new byte[(int) piece.getLength()];
+
+
+//           is.readNBytes(data,0, (int) piece.getLength());
+           piece.data = data;
+//           fileWriter.write(piece.data); // todo: eto toje v failovy tred pihnut'
+           FileWorker.invokeLater(new RecordPieceTask(piece,fileName));
+    }
+
+//       catch (IOException e) {
+//           e.printStackTrace();
+//           System.out.println("heh'");
+//           System.exit(0);
+//       }
+
+
+
+    private void sendInfoMessage(SocketChannel sc) {
+        System.out.println(fileName);
+        ByteBuffer a = ByteBuffer.allocate(4);
+        a.putInt(fileName.length());
+        a.rewind();
+        try {
+            sc.write(a);
+            sc.write(ByteBuffer.wrap(fileName.getBytes()));
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void receiveMessage(SocketChannel sc) {
+        ByteBuffer size = ByteBuffer.allocate(4);
+        try {
+            sc.read(size);
+            size.rewind();
+            int len  = size.getInt();
+            ByteBuffer msg = ByteBuffer.allocate(len);
+            sc.read(msg);
+            byte[] data = new byte[msg.position()];
+            msg.rewind();
+            msg.get(data);
+            String answer = new String(data);
+            System.out.println(answer);
+            parseMessage(answer,sc.getRemoteAddress().toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void parseMessage(String msg,String source) {
+        if (!tableOfAvailablePieces.containsKey(source))
+            tableOfAvailablePieces.put(source,new ArrayList<>());
+
+        switch (msg) {
+            case "ALL":
+                for (int i = 0; i < pieceNumber; i++) {
+                    tableOfAvailablePieces.get(source).add(i);
+                    System.out.println(tableOfAvailablePieces.get(source).get(i));
+                }
+                break;
+            case "NONE":
+                break;
+            default:
+                String [] data = msg.split(" ");
+                if (data[0].equals("PIECES")) {
+                    for (String s:data) {
+                        tableOfAvailablePieces.get(source).add(Integer.parseInt(s));
+                    }
+                }
+                else
+                    System.out.println("PRISLALI HUYNU!");
+        }
+    }
+
+    private int getNumberOfNextRecordedPiece(String src) {
+        Integer index;
+        do {
+             index = ThreadLocalRandom.current().nextInt(0,tableOfAvailablePieces.get(src).size()-1);
+        }while (!recordedPieces.get(index));
+
+        int pieceNumber = tableOfAvailablePieces.get(src).get(index);
+        recordedPieces.replace(pieceNumber,true);
+        return pieceNumber;
+    }
+
+    private Map<Integer,Boolean> recordedPieces = new HashMap<>();
+    private Map<String,List<Integer>> tableOfAvailablePieces = new HashMap<>();
+    private int pieceLength;
+    private String fileName;
+    private FileWorker fw;
+    private int pieceNumber;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+    есть тред "раздающий" - он долбится в ендлесс лупе и смотрит, не постучались ли  к нему
+    если постучались, он смотрит что именнно у него спрашивают - название файла, номер куска длину и стартпоинт
+    при на
+
+
+
+* */
---
+++ IncomingListener.java
@@ -0,0 +1,229 @@
+import java.io.*;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.channels.*;
+import java.util.*;
+
+public class IncomingListener implements Runnable {
+
+    private void sendInfoMessage(SocketChannel sc) {
+        String fileName = "bivis i batthhead";
+        System.out.println(fileName);
+        ByteBuffer a = ByteBuffer.allocate(4);
+        a.putInt(fileName.length());
+        a.rewind();
+        try {
+            sc.write(a);
+            sc.write(ByteBuffer.wrap(fileName.getBytes()));
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void run() {
+        try (InputStreamReader loadfile = new InputStreamReader(IncomingListener.class.getClassLoader().getResourceAsStream(propertiesFileName), "UTF-8")) {
+            pathesToFiles.load(loadfile);
+            Selector selector = Selector.open();
+            ServerSocketChannel ssc = ServerSocketChannel.open();
+            ssc.bind(new InetSocketAddress("localhost", PORT));
+            ssc.configureBlocking(false);
+            int ops = ssc.validOps();
+            ssc.register(selector, ops, null);
+            int connections;
+            ByteBuffer readBuffer = ByteBuffer.allocate(4);
+            SocketChannel is = null;
+            String keyIp = "null";
+            while (true) {
+                connections = selector.select(1000);
+//                System.out.println("zdras'te");
+
+//                System.out.println(heh.position());
+
+                if (connections == 0) {
+                    System.out.println("waiting for peer");
+                    System.out.println("waiting for peer");
+                    continue;
+                }
+                Set<SelectionKey> keys = selector.selectedKeys();
+                Iterator<SelectionKey> it = keys.iterator();
+
+                while (it.hasNext()) {
+//                    System.out.println("y menya est' next!");
+                    SelectionKey key = it.next();
+                    if (key.isAcceptable()) {
+                        SocketChannel socket = ssc.accept();
+                        keyIp = socket.getRemoteAddress().toString();
+                        peersStatus.put(keyIp, true);
+                        socket.configureBlocking(false);
+                        socket.register(selector, SelectionKey.OP_READ  | SelectionKey.OP_WRITE);
+                        System.out.println("got one!");
+                    }
+//                    if (key.) {
+//                        System.out.println("ya ne validny!");
+//                        keys.remove(key);
+//                    }
+                    if (key.isReadable()) {
+                        System.out.println("YA READEBLE!");
+                        if (peersStatus.get(keyIp)) {
+                            is = (SocketChannel) key.channel();
+                            System.out.println(is);
+                            int len = is.read(readBuffer);
+                            if (len==-1){
+                                System.out.println("chelik sdoh");
+                                key.cancel();
+                                break;
+                            }
+                            System.out.println(len);
+                            readBuffer.rewind();
+                            messageLen = readBuffer.getInt();
+                            heh = ByteBuffer.allocate(messageLen);
+                            System.out.println(messageLen);
+                            peersStatus.replace(keyIp, false);
+                            is.read(heh);
+                            System.out.println(heh);
+                        } else {
+                            if (heh.hasRemaining()) {
+                                System.out.println("vse ewe kachau!");
+                                is.read(heh);
+                                System.out.println(heh);
+                            } else if (heh.position() == messageLen) {
+                                System.out.println(heh);
+                                heh.clear();
+                                System.out.println("YA SKACHAL!");
+                                is.register(selector, SelectionKey.OP_WRITE);
+                                peersStatus.replace(keyIp, true);
+                            }
+                        }
+//                        System.out.println(len);
+//                        switch (len) {
+//                            case 0:
+//                                readBuffer.clear();
+//                                break;
+//                            case -1:
+//                                System.out.println("chelik sdoh");
+//                                is.close();
+//                                break;
+//                            default:
+//                                for (byte b: readBuffer.array()) {
+//                                    tmpBuffer.add(b);
+//                                }
+////                                System.out.println(new String(readBuffer.array()).trim());
+//                                if (len==ASYNC_BUFF_LENGTH) {
+//                                    readBuffer.clear();
+//                                }
+//                                else {
+//                                    System.out.println(tmpBuffer);
+//                                    tmpBuffer.clear();
+//                                    readBuffer.clear();
+//                                }
+//                        }
+
+//                        byte data[] = new byte[4];
+//                        readBuffer.get(data);
+//                        System.exit(0);
+//                        System.out.println(new String(data));
+//                        System.out.println("heh")
+                    }
+                    else if (key.isWritable()) {
+                        if (heh.position()==messageLen) {
+                            System.out.println("piwu!");
+                            String msg = new String(heh.array());
+                            String[] msgData = msg.split(" ");
+                            heh.clear();
+                            if (msgData.length==1) {
+                                if (pathesToFiles.containsKey(msg)) {
+                                    ByteBuffer x = ByteBuffer.allocate(4);
+                                    x.putInt("ALL".length());
+                                    x.rewind();
+                                    is.write(x);
+                                    is.write(ByteBuffer.wrap("ALL".getBytes()));
+                                }
+                                else {
+                                    ByteBuffer x = ByteBuffer.allocate(4);
+                                    x.putInt("NONE".length());
+                                    x.rewind();
+                                    is.write(x);
+                                    is.write(ByteBuffer.wrap("NONE".getBytes()));
+                                }
+                            }
+                        }
+//                        is = (SocketChannel)key.channel();
+//                       if (is.write(ByteBuffer.wrap("sosi huy! like esli bi(d)lo".getBytes()))==0) {
+//                           System.out.println("can't write!");
+//                           key.cancel();
+//                       }
+//                        try {
+//                            Thread.sleep(5000);
+//                        } catch (InterruptedException e) {
+//                            e.printStackTrace();
+                    }
+////
+                }
+                it.remove();
+            }
+//               try (var is = new BufferedReader(new InputStreamReader(client.getInputStream()));
+//                    var os = new PrintWriter(new OutputStreamWriter( client.getOutputStream()))) {
+//                  var request = is.readLine();
+//                  System.out.println(request);
+//                  String infos[] = request.split(" ");
+//                  int pieceLengt = Integer.parseInt(infos[2]);
+//                  if (pathesToFiles.containsKey(infos[0]) || pathesToFiles.containsKey(infos[0]+infos[1])) {
+//                      //os.println("shas razdam");
+//                      String path = pathesToFiles.getProperty(infos[0]);
+//                      String file = (path + infos[0]);
+//                      System.out.println(file);
+//                      RandomAccessFile fileToGive = new RandomAccessFile(file,"r");
+//                      fileToGive.seek(Long.parseLong(infos[1])*pieceLengt);
+//                      var fileData = new byte[pieceLengt];
+//                      fileToGive.read(fileData,0,pieceLengt);
+////                      client.getOutputStream().write(fileData);
+//                      os.write(Arrays.toString(fileData));
+//                  }
+//                   else  {
+//                       os.println("hule tebe nado y menya nichago net");
+//                       os.flush();
+//                   }
+//               }
+////               catch () {
+////                   System.out.println("powel naghuy");
+////                   exc.printStackTrace();
+////                   System.exit(0);
+////               }
+//               catch (SocketTimeoutException exc) {
+//                   System.out.println("Client umer!");
+//               }
+//
+//
+//            }
+//        }
+//        catch (IOException exc) {
+//            exc.printStackTrace();
+//            System.out.println("y tebya chot s sdiom daunom");
+//            System.exit(0);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void startListening() {
+    }
+
+    private ByteBuffer heh = ByteBuffer.allocate(4);
+    private final int ASYNC_BUFF_LENGTH = 1024;
+    private List<Byte> tmpBuffer = new ArrayList<>();
+    private Map<String, ArrayList<Integer>> tableOfAvailability = new HashMap<>();
+    private Map <String,Boolean> peersStatus = new HashMap<>();
+    private List<Integer> myPieces = new ArrayList<>();
+    private int messageLen =-1;
+    private final  int PORT = 9875;
+    private Properties pathesToFiles = new Properties();
+    private final String propertiesFileName = "files.properties";
+}
